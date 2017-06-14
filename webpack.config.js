737274de0605be8f8f7c33f6f8db06781f89470d
grullo90 <g.rullo@expandjs.com>
2017-06-14 15:53:29 +0200

    Release 1.1.1

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 // Const
-const webpack = require('webpack');
+const Uglify = require('uglifyjs-webpack-plugin');
 
 // Exporting
 module.exports = {
@@ -14,9 +14,9 @@ module.exports = {
     },
     output: {
         filename: 'xp-uploader.js',
-        path: './dist'
+        path: `${__dirname}/dist`
     },
     plugins: [
-        new webpack.optimize.UglifyJsPlugin({compress: {warnings: false}, output: {comments: false}})
+        new Uglify({compress: {warnings: false}, output: {comments: false}})
     ]
 };
